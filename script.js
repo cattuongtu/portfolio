0c3fccc8d558741fe7b60993bb369941d8c9e25f
Cat-Tuong Tu <cattuongtu3@gmail.com>
2020-09-27 16:35:05 -0700

    Used downnload attribute for html instead of script file.

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-let downloadButton = document.querySelector(
-	"#download-btn"
-);
-downloadButton.addEventListener("click", function () {
-	downloadButton.download = "my-resume.pdf";
-	downloadButton.dispatchEvent(new MouseEvent("click"));
-});
+
